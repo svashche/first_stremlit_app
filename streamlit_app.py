446cb9bd7bdfceebee7abfccb4d28c3df3302346
Serge <52250880+svashche@users.noreply.github.com>
2023-01-30 15:57:25 -0700

    Update streamlit_app.py
    New menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('nothing is finer than your diner')
+
+streamlit.title('My Parents Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blue Berry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
