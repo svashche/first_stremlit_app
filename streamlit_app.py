e41d07dfb0854b8a64fc20b5ea63586ed1c9e8be
Serge <52250880+svashche@users.noreply.github.com>
2023-01-30 17:02:05 -0700

    Update streamlit_app.py
    Added pandas list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,3 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
